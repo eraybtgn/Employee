5205dd1e8f6a40c8f0c366692b2c631a605c3c27
erayBatigun <eraybtgn24@gmail.com>
2022-10-14 15:25:58 +0300

    First commit

---
+++ src/Emp.java
@@ -0,0 +1,13 @@
+public class Emp {
+    public static void main(String[] args) {
+    Maas emp1 = new Maas("Eray","Batıgün",2800,45,2017);
+    emp1.tax(emp1.salary);
+    emp1.bonus(emp1.workHours);
+    emp1.raiseSalary(emp1.hireYear);
+
+        System.out.println(emp1);
+
+    }
+
+
+}
---
+++ src/Maas.java
@@ -0,0 +1,61 @@
+public class Maas {
+    String name;
+    String surname;
+    int salary;
+    int workHours;
+    int hireYear;
+
+
+    public Maas(String name, String surname, int salary, int workHours, int hireYear) {
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
+
+    public int tax(int salary){
+        if(salary>1000){
+            this.salary=salary-(salary*3/100);
+            System.out.println("Vergisi düşülmüş yeni maaş: "+this.salary+"TL");
+        }else{
+            System.out.println("Yeterli gelir olmadığı için maaşa vergi uygulanmayacak.");
+        }
+        return this.salary;
+    }
+    public int bonus(int workHours){
+        if (workHours>40){
+            salary+=(((workHours-40)*30)*4);  //*4 diyerek 4 haftalık fazla mesai maaşa eklendi
+            System.out.println("Haftalık fazla mesai geliri: "+((workHours-40)*30)+"TL");
+        }else{
+            System.out.println("Fazla mesai bulunumadı..");
+        }
+        return salary;
+    }
+
+    public int raiseSalary(int hireYear){
+        int today=2021;
+        if (today-hireYear<10){
+            System.out.println("Yeni zam miktarı: "+((salary*5)/100)+"TL");
+            salary+=(salary*5)/100;
+        } else if (today-hireYear>9 && hireYear-today<20) {
+            System.out.println("Yeni zam miktarı: "+((salary*10)/100)+"TL");
+            salary+=(salary*10)/100;
+        } else if (today-hireYear>19) {
+            System.out.println("Yeni zam miktarı: "+((salary*15)/100)+"TL");
+            salary+=(salary*15)/100;
+        }
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Calisan :" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                ", workHours=" + workHours +
+                ", hireYear=" + hireYear +
+                '}';
+    }
+}
